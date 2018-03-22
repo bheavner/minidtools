940d9ac9fed320e6bff70338037d4161e3e165f0
bheavner <bheavner@gmail.com>
2018-03-21 22:18:29 -0700

    adding a function for coverage and testing

---
+++ R/foo.R
@@ -0,0 +1,7 @@
+#' A meaningless function to test package infrastructure
+#'
+#' @export
+
+foo <- function(){
+  return(TRUE)
+}
---
+++ tests/testthat/test-foo.R
@@ -0,0 +1,5 @@
+context("test_foo - unit tests")
+
+test_that("foo() returns true", {
+  expect_true(foo())
+})
