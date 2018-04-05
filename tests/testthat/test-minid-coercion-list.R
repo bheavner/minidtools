bba67b6dbe49a9008d2a35b65e37e4b421a5af35
Ben Heavner <bheavner@gmail.com>
2018-04-05 14:48:49 -0700

    adding coercion and show tests

---
+++ tests/testthat/test-minid-coercion-list.R
@@ -0,0 +1,22 @@
+context("test_minid_coercion to list - unit tests")
+
+test_that("as.list(minid) returns a list", {
+  load("demo.rda")
+  expected_regex <-
+    paste0(
+      "MINID:\n  identifier = ark:/57799/b9j69h\n  ",
+      "creator = mdarcy (<id string>)\n  ",
+      "created = Mon, 19 Mar 2018 17:43:57 GMT\n  ",
+      "checksum = ",
+      "6484968f81afac84857d02b573b0d589fb2f9582a2b920572830dc5781e0a53c\n  ",
+      "checksum_function = SHA\n  ",
+      "status = ACTIVE\n  ",
+      "locations = mdarcy - ",
+      "https://nih-commons.s3.amazonaws.com/misc/agr-example.tgz\n  ",
+      "title = mdarcy - AGR Data set with identifier-based references to ",
+      "data in cloud storage\n  ",
+      "obsoleted_by = \n  ",
+      "content_key = "
+    )
+  expect_output(show(demo), regexp = expected_regex, fixed = TRUE)
+})
---
+++ tests/testthat/test-minid-show.R
@@ -0,0 +1,6 @@
+context("test_minid_coercion to list - unit tests")
+
+test_that("as.list(minid) returns a list", {
+  load("demo.rda")
+  expect_true(is.list(as.list(demo)))
+})
