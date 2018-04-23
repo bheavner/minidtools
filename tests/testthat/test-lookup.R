6358954f99737b1afc36e7f5da7765729d62801e
Ben Heavner <bheavner@gmail.com>
2018-04-23 14:38:36 -0700

    lookup by hash working

--- tests/testthat/test-lookup.R
+++ tests/testthat/test-lookup.R
@@ -1,28 +1,32 @@
-context("test_minid_show - unit tests")
-
-test_that("show(minid) prints expected output", {
-  load("demo.rda")
-  expected_regex <-
-    paste0(
-      "MINID:\n  ",
-      "identifier = ark:/57799/b9j69h\n  ",
-      "short_identifier = minid:b9j69h\n  ",
-      "creator = mdarcy\n  ",
-      "orcid = <id string>\n  ",
-      "created = Mon, 19 Mar 2018 17:43:57 GMT\n  ",
-      "checksum = 6484968f81afac84857d02b573b0d589fb2f9582a2b920572830dc578",
-      "1e0a53c\n  ",
-      "checksum_function = SHA\n  ",
-      "status = ACTIVE\n  ",
-      "locations = https://nih-commons.s3.amazonaws.com/misc/agr-example.tgz",
-      "\n    ",
-      "(use locations(object) for more)\n  ",
-      "titles = AGR Data set with identifier-based references todata in ",
-      "cloud storage\n    ",
-      "(use titles(object) for more)\n  ",
-      "obsoleted_by = \n    ",
-      "(use obsoleted_by(object) for more)\n  ",
-      "content_key = "
-    )
-  expect_output(show(demo), regexp = expected_regex, fixed = TRUE)
+context("test_lookup - unit tests")
+#' my_minid <- lookup(query = "ark:/57799/b9j69h", configuration = config)
+#' my_minid <- lookup(query = "minid:b9j69h", configuration = config)
+#' my_minid <- lookup(query = "file:./some/file.RDa", algo = "md5",
+#'                    configuration = config)
+#'
+test_that("lookup('ark:/57799/b9j69h') returns expected minid", {
+  load("demo-config.rda")
+  config <- demo_config
+  expect_identical(
+    identifier(lookup("ark:/57799/b9j69h", configuration = config)),
+    "ark:/57799/b9j69h")
 })
+
+test_that("lookup('minid:b9j69h') returns expected minid", {
+  load("demo-config.rda")
+  config <- demo_config
+  expect_identical(
+    identifier(lookup("minid:b9j69h", configuration = config)),
+    "ark:/57799/b9j69h")
+})
+
+test_that("lookup(checksum) returns expected minid", {
+  load("demo-config.rda")
+  config <- demo_config
+  checksum <- "6484968f81afac84857d02b573b0d589fb2f9582a2b920572830dc5781e0a53c"
+  expect_identical(
+    identifier(lookup(checksum, configuration = config)),
+    "ark:/57799/b9j69h")
+})
+
+# TODO - add test for file-based lookup after I mint a minid for one
